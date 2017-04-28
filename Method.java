542e17c11799a7862abf5482c90c5a382014d4f6
Eugene <evasquez184@student.alamo.edu>
2017-04-27 21:53:44 -0500

    Create Method.java

---
+++ Method.java
@@ -0,0 +1,33 @@
+//Eugene Vasquez
+//ITSE
+//April 27, 2017
+//Calculates average for up to 4 doubles
+
+public class Method
+{
+public static void main(String[]strArgs) {
+
+double num1 = 1;
+double num2 = 2;
+double num3 = 3;
+double num4 = 4;
+
+  public average ( num1, num2);
+     {
+        return ((num1+num2) / 2.0);
+     }
+
+  public average ( num1, num2, num3);
+     {
+        return ((num1+num2+num3) / 3.0);
+     }
+ 
+  public average ( num1, num2, num3,                       
+   num4);
+     {
+        return ((num1+num2+num3+num4) / 4.0);
+     }
+
+   }
+
+}
